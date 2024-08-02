29a76944dae92460a1be814969a8832f7d334934
Aryadani10rpl7 <aryadani_santoso_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:56:01 +0700

    Add files via upload

---
+++ Abc.java
@@ -0,0 +1,6 @@
+public class Abc {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Aryadani putra santoso dari kelas 10 rpl 7 yang berasal dari kota Blitar");
+        String a = "1"; 
+    }
+}
